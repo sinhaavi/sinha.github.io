7fd2062e4a45a2703c8f2a484ba9bdb4859aca8e
sinhaavi <aviapratim@gmail.com>
2021-03-28 23:18:47 +0530

    website

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scollY >20){
+            $(".navbar").addClass("sticky");
+
+        }else{
+
+        }
+    })
+
+});
